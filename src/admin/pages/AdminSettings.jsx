21c43413516f19d5f27898693100b84a7da6b496
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-23 09:13:27 +0530

    Almost done with user side frontend

--- src/admin/pages/AdminSettings.jsx
+++ src/admin/pages/AdminSettings.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
 const AdminSettings = () => {
+  const [openUsername, setOpenUsername] = useState(false);
+  const [openEmail, setOpenEmail] = useState(false);
   return (
     <>
       <div className="flex h-screen" id="root">
@@ -14,39 +16,46 @@ const AdminSettings = () => {
               <h3 className="text-xl font-semibold mb-6 text-gray-800">
                 Account Security Settings
               </h3>
-              <div className="border border-gray-200 rounded-lg mb-4">
+              <div className="border-2 border-gray-200 rounded-lg mb-4">
                 <button
+                  onClick={() => setOpenUsername(!openUsername)}
                   className="w-full flex justify-between items-center p-4 focus:outline-none"
-                  onclick="toggleAccordion('username')"
                 >
                   <span className="text-lg font-medium text-gray-800">
                     Username
                   </span>
                   <span
-                    className="material-icons text-gray-600 transform transition-transform duration-300"
-                    id="username-arrow"
+                    className={`material-icons text-gray-600 transform transition-transform duration-300 ${
+                      openUsername && "rotate-180"
+                    }`}
                   >
                     expand_more
                   </span>
                 </button>
-                <div className="px-4 pb-4 " id="username-content">
-                  <div className="border-t border-gray-200  pt-4">
+
+                <div
+                  className={`px-4 overflow-hidden transition-all duration-400 ease-in-out  ${
+                    openUsername ? "max-h-[1000px]" : "max-h-0"
+                  }`}
+                >
+                  <div className="border-t border-gray-200 pt-4">
                     <div className="mb-4">
                       <label
-                        className="block text-sm font-medium text-gray-700  mb-1"
-                        for="new-username"
+                        className="block text-sm font-medium text-gray-700 mb-1"
+                        htmlFor="new-username"
                       >
                         New Username
                       </label>
                       <input
-                        className="mt-1 block w-full p-2 rounded-md border-gray-300 shadow-sm text-gray-500 focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+                        className="mt-1 block w-full p-2 rounded-md outline-none border border-gray-300 shadow-sm text-gray-800 focus:border-teal-500 focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"
                         id="new-username"
                         name="new-username"
                         placeholder="Enter new username"
                         type="text"
                       />
                     </div>
-                    <div className="flex justify-between items-center">
+
+                    <div className="flex justify-between items-center pb-4">
                       <button className="px-4 py-2 bg-teal-500 text-white rounded-md hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50">
                         Update Username
                       </button>
@@ -54,22 +63,27 @@ const AdminSettings = () => {
                   </div>
                 </div>
               </div>
-              <div className="border border-gray-200 rounded-lg mb-4">
+              <div className="border-2 border-gray-200 rounded-lg mb-4">
                 <button
                   className="w-full flex justify-between items-center p-4 focus:outline-none"
-                  onclick="toggleAccordion('email')"
+                  onClick={() => setOpenEmail(!openEmail)}
                 >
                   <span className="text-lg font-medium text-gray-800 ">
                     Email Address
                   </span>
                   <span
-                    className="material-icons text-gray-600 transform transition-transform duration-300"
-                    id="email-arrow"
+                    className={`material-icons text-gray-600 transform transition-transform duration-300 ${
+                      openEmail && "rotate-180"
+                    }`}
                   >
                     expand_more
                   </span>
                 </button>
-                <div className="px-4 pb-4" id="email-content">
+                <div
+                  className={`px-4 overflow-hidden transition-all duration-400 ease-in-out  ${
+                    openEmail ? "max-h-[1000px]" : "max-h-0"
+                  }`}
+                >
                   <div className="border-t border-gray-200 pt-4">
                     <div className="mb-4">
                       <label
@@ -79,7 +93,7 @@ const AdminSettings = () => {
                         New Email
                       </label>
                       <input
-                        className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm text-gray-500 focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+                        className="mt-1 p-2 block w-full rounded-md outline-none border-gray-300 shadow-sm text-gray-500 focus:border-teal-500 focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"
                         id="new-email"
                         name="new-email"
                         placeholder="Enter new email address"
@@ -119,7 +133,7 @@ const AdminSettings = () => {
                         Current Password
                       </label>
                       <input
-                        className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring focus:ring-teal-500 text-gray-500 focus:ring-opacity-50"
+                        className="mt-1 p-2 block w-full outline-none rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-2 focus:ring-teal-500 text-gray-500 focus:ring-opacity-50"
                         id="current-password"
                         name="current-password"
                         placeholder="Enter current password"
@@ -134,7 +148,7 @@ const AdminSettings = () => {
                         New Password
                       </label>
                       <input
-                        className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring text-gray-500 focus:ring-teal-500 focus:ring-opacity-50 "
+                        className="mt-1 p-2 block w-full outline-none rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-2 text-gray-500 focus:ring-teal-500 focus:ring-opacity-50 "
                         id="new-password"
                         name="new-password"
                         placeholder="Enter new password"
@@ -149,7 +163,7 @@ const AdminSettings = () => {
                         Confirm New Password
                       </label>
                       <input
-                        className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm text-gray-500  focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50 "
+                        className="mt-1 p-2 block w-full outline-none rounded-md border-gray-300 shadow-sm text-gray-500  focus:border-teal-500 focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50 "
                         id="confirm-password"
                         name="confirm-password"
                         placeholder="Confirm new password"
