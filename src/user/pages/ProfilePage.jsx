f5db36ca70994b02fc75511e8872270f80527875
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-29 12:55:22 +0530

    Updated

--- src/user/pages/ProfilePage.jsx
+++ src/user/pages/ProfilePage.jsx
@@ -15,8 +15,12 @@ const ProfilePage = () => {
             <div className="w-32 h-32 rounded-full bg-gray-200 mx-auto mb-6 flex items-center justify-center overflow-hidden">
               <FaRegCircleUser className="text-8xl text-gray-700" />
             </div>
-            <h2 className="text-xl md:text-3xl font-bold text-gray-800  mb-2">John Doe</h2>
-            <p className="text-gray-500 text-base md:text-lg">johndoe@example.com</p>
+            <h2 className="text-xl md:text-3xl font-bold text-gray-800  mb-2">
+              John Doe
+            </h2>
+            <p className="text-gray-500 text-base md:text-lg">
+              johndoe@example.com
+            </p>
           </div>
           <div className="space-y-6">
             <h3 className="text-lg md:text-2xl font-semibold text-gray-800">
@@ -67,7 +71,7 @@ const ProfilePage = () => {
               </div>
               <div className="relative">
                 <label
-                  className="block text-sm font-medium text-gray-700 mb-1"
+                  className="block text-xs md:text-sm font-medium text-gray-700 mb-1"
                   htmlFor="address"
                 >
                   Address
@@ -80,7 +84,12 @@ const ProfilePage = () => {
                 />
               </div>
             </div>
-            <div className="flex justify-end pt-4">
+            <div className="flex flex-col md:flex-row items-end gap-2 justify-end pt-4">
+              <Link to={"/"}>
+                <button className="px-6 py-3 bg-gray-600 text-white rounded-md font-semibold hover:bg-gray-700 transition-colors duration-300 ease-in-out shadow-md">
+                  Cancel
+                </button>
+              </Link>
               <Link to={"/"}>
                 <button className="px-6 py-3 bg-teal-600 text-white rounded-md font-semibold hover:bg-teal-700 transition-colors duration-300 ease-in-out shadow-md">
                   Save Changes
