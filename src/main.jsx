3a8ac9cd724e4bbbb907ec4f07351cfc144f3180
sayeedmunees <sayeedmunees0@gmail.com>
2026-03-29 20:08:52 +0530

    feat: Updated .env

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId="230848631845-cktb2jh7d23823qoank9einl38lf4m18.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
     </BrowserRouter>
--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,1 +1,1 @@
-export const serverURL = "http://localhost:4000"
+export const serverURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
